d4392f5dd473ec27c6e2634a63dc871cd7e051df
Chandrika3Bhat <151983420+Chandrika3Bhat@users.noreply.github.com>
2024-04-29 20:46:23 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+//Footer.js
+
+import React from 'react';
+
+export default function Footer() {
+	return (
+		<div className='journal-footer'>
+			<caption> 2024 |</caption>
+			<caption className='footer-caption'>THANKS FOR VISITING </caption>
+		</div>
+	)
+};
